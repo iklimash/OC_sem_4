1bdca092b00a6247e1c607ee74cf443781dc94b5
iklimash <ivklimash@stud.etu.ru>
2026-02-11 07:03:24 -0500

    v2

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,62 +1,79 @@
+#define _GNU_SOURCE             /* требуется для gettid() */
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/syscall.h>        /* для syscall() */
+#include <sys/types.h>
 
 typedef struct
 {
-    int flag;   /* флаг завершения работы потока */
-    char sym;   /* символ, выводимый потоком */
+    int flag;           /* флаг завершения работы потока */
+    char sym;           /* символ, выводимый потоком */
+    pthread_t tid;      /* идентификатор pthread_self() */
+    pid_t linux_tid;    /* идентификатор нити (gettid) */
 } targs;
-
 
 /* Поточная функция №1 */
 void* proc1(void* arg1) 
 {
-    printf("поток 1 начал работу\n"); 
+    printf("поток 1 начал работу\n");
 
     targs* arg = (targs*) arg1; /* приведение универсального указателя к типу targs */
 
-    while (arg->flag == 0)      /* цикл выполняется, пока флаг не установлен */
+    arg->tid = pthread_self();  /* получение идентификатора POSIX-потока */
+    arg->linux_tid = syscall(SYS_gettid); /* получение идентификатора нити ядра */
+
+    printf("поток 1: pthread_self() = %lu\n", (unsigned long)arg->tid);
+    printf("поток 1: gettid() = %d\n", arg->linux_tid);
+    printf("поток 1: getpid() = %d\n", getpid()); /* получение PID процесса */
+
+    while (arg->flag == 0)
     {
-        printf("%c", arg->sym); /* вывод символа в стандартный поток вывода */
-        fflush(stdout);         /* принудительная очистка буфера stdout */
-        sleep(1);               /* приостановка выполнения потока на 1 секунду */
+        printf("%c", arg->sym);
+        fflush(stdout); /* принудительная очистка буфера stdout */
+        sleep(1);       /* приостановка выполнения на 1 секунду */
     }
 
-    printf("\nпоток 1 закончил работу\n"); 
+    printf("\nпоток 1 закончил работу\n");
 
-    pthread_exit((void*)111); /* завершение потока и возврат кода завершения 111 */
+    pthread_exit((void*)111); /* завершение потока */
 }
-
 
 /* Поточная функция №2 */
 void* proc2(void* arg2) 
 {
-    printf("поток 2 начал работу\n"); 
-    targs* arg = (targs*) arg2; /* приведение универсального указателя к типу targs */
+    printf("поток 2 начал работу\n");
 
-    while (arg->flag == 0)      /* цикл выполняется, пока флаг не установлен */
+    targs* arg = (targs*) arg2;
+
+    arg->tid = pthread_self();  /* получение идентификатора POSIX-потока */
+    arg->linux_tid = syscall(SYS_gettid); /* получение идентификатора нити ядра */
+
+    printf("поток 2: pthread_self() = %lu\n", (unsigned long)arg->tid);
+    printf("поток 2: gettid() = %d\n", arg->linux_tid);
+    printf("поток 2: getpid() = %d\n", getpid()); /* получение PID процесса */
+
+    while (arg->flag == 0)
     {
-        printf("%c", arg->sym); /* вывод символа в стандартный поток вывода */
-        fflush(stdout);         /* принудительная очистка буфера stdout */
-        sleep(1);               /* приостановка выполнения потока на 1 секунду */
+        printf("%c", arg->sym);
+        fflush(stdout);
+        sleep(1);
     }
 
     printf("\nпоток 2 закончил работу\n");
 
-    pthread_exit((void*)222); /* завершение потока и возврат кода завершения 222 */
+    pthread_exit((void*)222);
 }
-
 
 int main(void) 
 {
     printf("программа начала работу\n");
 
-    pthread_t id1, id2; /* идентификаторы потоков */
-    targs args1, args2; /* структуры аргументов потоков */
+    pthread_t id1, id2; /* идентификаторы потоков, полученные pthread_create */
+    targs args1, args2;
 
-    void* exitCode1;    /* переменная для получения кода завершения первого потока */
-    void* exitCode2;    /* переменная для получения кода завершения второго потока */
+    void* exitCode1;
+    void* exitCode2;
 
     args1.flag = 0; 
     args1.sym = '1';
@@ -64,43 +81,55 @@ int main(void)
     args2.flag = 0; 
     args2.sym = '2';
 
-    /* создание первого потока:
-       id1 – дескриптор потока,
-       NULL – атрибуты по умолчанию,
-       proc1 – поточная функция,
-       &args1 – передаваемые параметры */
-    int rv1 = pthread_create(&id1, NULL, proc1, &args1);
+    printf("main: getpid() = %d\n", getpid()); /* получение идентификатора процесса */
+    printf("main: gettid() = %ld\n", syscall(SYS_gettid)); /* идентификатор нити main */
 
-    /* создание второго потока:
-       id2 – дескриптор потока,
-       NULL – атрибуты по умолчанию,
-       proc2 – поточная функция,
-       &args2 – передаваемые параметры */
-    int rv2 = pthread_create(&id2, NULL, proc2, &args2);
+    int rv1 = pthread_create(&id1, NULL, proc1, &args1); 
+    /* создание первого потока */
 
-    printf("программа ждет нажатия клавиши\n"); 
+    int rv2 = pthread_create(&id2, NULL, proc2, &args2); 
+    /* создание второго потока */
 
-    getchar(); /* ожидание нажатия клавиши (чтение символа из stdin) */
+    sleep(2); /* задержка для гарантии инициализации tid в потоках */
 
-    printf("клавиша нажата\n"); 
+    printf("\nСравнение идентификаторов:\n");
 
-    args1.flag = 1; /* установка флага завершения первого потока */
-    args2.flag = 1; /* установка флага завершения второго потока */
+    printf("id1 (pthread_create) = %lu\n", (unsigned long)id1);
+    printf("tid1 (pthread_self)  = %lu\n", (unsigned long)args1.tid);
 
-    /* ожидание завершения первого потока:
-       id1 – идентификатор потока,
-       &exitCode1 – получение кода завершения */
-    pthread_join(id1, &exitCode1);
+    printf("id2 (pthread_create) = %lu\n", (unsigned long)id2);
+    printf("tid2 (pthread_self)  = %lu\n", (unsigned long)args2.tid);
 
-    /* ожидание завершения второго потока:
-       id2 – идентификатор потока,
-       &exitCode2 – получение кода завершения */
-    pthread_join(id2, &exitCode2);
+    /* сравнение id1 и tid1 */
+    if (pthread_equal(id1, args1.tid))
+        printf("id1 и pthread_self() потока 1 совпадают\n");
+    else
+        printf("id1 и pthread_self() потока 1 НЕ совпадают\n");
 
-    printf("код завершения первого потока: %ld\n", (long)exitCode1); /* вывод кода завершения */
-    printf("код завершения второго потока: %ld\n", (long)exitCode2); /* вывод кода завершения */
+    /* сравнение id2 и tid2 */
+    if (pthread_equal(id2, args2.tid))
+        printf("id2 и pthread_self() потока 2 совпадают\n");
+    else
+        printf("id2 и pthread_self() потока 2 НЕ совпадают\n");
 
-    printf("программа завершила работу\n"); /* сообщение о завершении программы */
+    /* сравнение идентификаторов разных потоков */
+    if (pthread_equal(id1, id2))
+        printf("id1 и id2 совпадают (ошибка)\n");
+    else
+        printf("id1 и id2 различны (корректно)\n");
 
-    return 0; /* корректное завершение функции main */
+    printf("программа ждет нажатия клавиши\n");
+
+    getchar(); /* ожидание ввода */
+
+    printf("клавиша нажата\n");
+
+    args1.flag = 1;
+    args2.flag = 1;
+
+    pthread_join(id1, &exitCode1); /* ожидание завершения потока 1 */
+    pthread_join(id2, &exitCode2); /* ожидание завершения потока 2 */
+
+    printf("код завершения первого потока: %ld\n", (long)exitCode1);
+    printf("код завершения второго потока: %ld\n", (long)exitCode2);
 }
