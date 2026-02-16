92e2310593f778532b1eec4d35530cf5ae63076a
iklimash <ivklimash@stud.etu.ru>
2026-02-16 04:13:50 -0500

    /

--- lab2/lab2_3/lab2_3.c
+++ lab2/lab2_3/lab2_3.c
@@ -4,53 +4,63 @@
 #include <pthread.h>
 
 int flag1 = 0, flag2 = 0;
-pthread_spinlock_t spin;   // спинлок
+pthread_spinlock_t spin;
 
 void* proc1(void* args) 
 {
     while (flag1 == 0) 
     {
-        pthread_spin_lock(&spin);   // захват
+        // ждём захвата спинлока
+        while (pthread_spin_trylock(&spin) != 0) {
+            usleep(1000); // короткая пауза 1 мс, чтобы не крутить CPU
+        }
+
+        // захватили — выводим блок
         for (int i = 0; i < 10; ++i) 
         {
             printf("%c", '1');
             fflush(stdout);
             sleep(1);
         }
-        pthread_spin_unlock(&spin); // освобождение
+
+        pthread_spin_unlock(&spin); // освобождаем
         sleep(1);
     }
-    pthread_exit(NULL);
+    return NULL;
 }
 
 void* proc2(void* args) 
 {
     while (flag2 == 0) 
     {
-        pthread_spin_lock(&spin);   // захват
+        while (pthread_spin_trylock(&spin) != 0) 
+        {
+            usleep(1000);
+        }
+
         for (int j = 0; j < 10; ++j) 
         {
             printf("%c", '2');
             fflush(stdout);
             sleep(1);
         }
-        pthread_spin_unlock(&spin); // освобождение
+
+        pthread_spin_unlock(&spin);
         sleep(1);
     }
-    pthread_exit(NULL);
+    return NULL;
 }
 
 int main() {
     pthread_t id1, id2;
 
-    // инициализация спинлока
     pthread_spin_init(&spin, PTHREAD_PROCESS_PRIVATE);
 
     pthread_create(&id1, NULL, proc1, NULL);
     pthread_create(&id2, NULL, proc2, NULL);
 
     getchar();
-    puts("Клавиша нажата.\r\n");
+    puts("\nКлавиша нажата.");
 
     flag1 = 1;
     flag2 = 1;
@@ -58,8 +68,6 @@ int main() {
     pthread_join(id1, NULL);
     pthread_join(id2, NULL);
 
-    // уничтожение спинлока
     pthread_spin_destroy(&spin);
-
     return 0;
 }
---
+++ lab2/lab2_3/lab2_3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcc -c lab2_3.c
+gcc -o lab2_3 lab2_3.o -lpthread
+./lab2_3
