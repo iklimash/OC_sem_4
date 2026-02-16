e07b6f35eb0cd12bef239ff1a74ebe65abdcf0ff
iklimash <ivklimash@stud.etu.ru>
2026-02-16 04:21:33 -0500

    2

--- lab2/lab2_2/lab2_2.c
+++ lab2/lab2_2/lab2_2.c
@@ -1,62 +1,64 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE                 // включение расширений GNU для POSIX
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 
-int flag1 = 0, flag2 = 0;
-pthread_spinlock_t spin;   // спинлок
+int flag1 = 0, flag2 = 0;           // флаги для управления завершением потоков
+pthread_spinlock_t spin;            // объявление спинлока
 
+// Функция потока 1
 void* proc1(void* args) 
 {
-    while (flag1 == 0) 
+    while (flag1 == 0)              // пока флаг завершения не установлен
     {
-        pthread_spin_lock(&spin);   // захват
+        pthread_spin_lock(&spin);   // захват спинлока для синхронизации
         for (int i = 0; i < 10; ++i) 
         {
-            printf("%c", '1');
-            fflush(stdout);
-            sleep(1);
+            printf("%c", '1');      // вывод символа '1'
+            fflush(stdout);          // немедленный вывод на экран
+            sleep(1);                // задержка 1 секунда
         }
-        pthread_spin_unlock(&spin); // освобождение
-        sleep(1);
+        pthread_spin_unlock(&spin); // освобождение спинлока
+        sleep(1);                    // дополнительная пауза перед следующей итерацией
     }
-    pthread_exit(NULL);
+    pthread_exit(NULL);             // завершение потока
 }
 
+// Функция потока 2
 void* proc2(void* args) 
 {
-    while (flag2 == 0) 
+    while (flag2 == 0)              // пока флаг завершения не установлен
     {
-        pthread_spin_lock(&spin);   // захват
+        pthread_spin_lock(&spin);   // захват спинлока для синхронизации
         for (int j = 0; j < 10; ++j) 
         {
-            printf("%c", '2');
-            fflush(stdout);
-            sleep(1);
+            printf("%c", '2');      // вывод символа '2'
+            fflush(stdout);          // немедленный вывод на экран
+            sleep(1);                // задержка 1 секунда
         }
-        pthread_spin_unlock(&spin); // освобождение
-        sleep(1);
+        pthread_spin_unlock(&spin); // освобождение спинлока
+        sleep(1);                    // дополнительная пауза перед следующей итерацией
     }
-    pthread_exit(NULL);
+    pthread_exit(NULL);             // завершение потока
 }
 
 int main() {
-    pthread_t id1, id2;
+    pthread_t id1, id2;             // идентификаторы потоков
 
     // инициализация спинлока
     pthread_spin_init(&spin, PTHREAD_PROCESS_PRIVATE);
 
-    pthread_create(&id1, NULL, proc1, NULL);
-    pthread_create(&id2, NULL, proc2, NULL);
+    pthread_create(&id1, NULL, proc1, NULL); // создание первого потока
+    pthread_create(&id2, NULL, proc2, NULL); // создание второго потока
 
-    getchar();
+    getchar();                       // ожидание нажатия клавиши пользователем
     puts("Клавиша нажата.\r\n");
 
-    flag1 = 1;
-    flag2 = 1;
+    flag1 = 1;                        // установка флага завершения потока 1
+    flag2 = 1;                        // установка флага завершения потока 2
 
-    pthread_join(id1, NULL);
-    pthread_join(id2, NULL);
+    pthread_join(id1, NULL);           // ожидание завершения потока 1
+    pthread_join(id2, NULL);           // ожидание завершения потока 2
 
     // уничтожение спинлока
     pthread_spin_destroy(&spin);
--- lab2/lab2_3/lab2_3.c
+++ lab2/lab2_3/lab2_3.c
@@ -1,43 +1,46 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE  // включаем расширения GNU, чтобы использовать usleep и другие функции
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 
-int flag1 = 0, flag2 = 0;
-pthread_spinlock_t spin;
+int flag1 = 0, flag2 = 0;  // флаги для остановки потоков
+pthread_spinlock_t spin;    // объявление спинлока
 
+// Функция для первого потока
 void* proc1(void* args) 
 {
-    while (flag1 == 0) 
+    while (flag1 == 0)  // основной цикл, пока флаг не установлен
     {
-        // ждём захвата спинлока
+        // пробуем захватить спинлок, пока не получится
         while (pthread_spin_trylock(&spin) != 0) {
-            usleep(1000); // короткая пауза 1 мс, чтобы не крутить CPU
+            usleep(1000); // ждем 1 мс, чтобы не перегружать CPU
         }
 
-        // захватили — выводим блок
+        // захватили спинлок — выводим символ '1' 10 раз
         for (int i = 0; i < 10; ++i) 
         {
-            printf("%c", '1');
-            fflush(stdout);
-            sleep(1);
+            printf("%c", '1');   // выводим символ
+            fflush(stdout);      // сбрасываем буфер вывода
+            sleep(1);            // пауза 1 секунда между символами
         }
 
-        pthread_spin_unlock(&spin); // освобождаем
-        sleep(1);
+        pthread_spin_unlock(&spin); // освобождаем спинлок
+        sleep(1); // небольшая пауза перед новой попыткой
     }
-    return NULL;
+    return NULL; // завершение потока
 }
 
+// Функция для второго потока (аналогично первому)
 void* proc2(void* args) 
 {
     while (flag2 == 0) 
     {
         while (pthread_spin_trylock(&spin) != 0) 
         {
-            usleep(1000);
+            usleep(1000); // ждем 1 мс перед повторной попыткой
         }
 
+        // захватили спинлок — выводим символ '2' 10 раз
         for (int j = 0; j < 10; ++j) 
         {
             printf("%c", '2');
@@ -45,29 +48,29 @@ void* proc2(void* args)
             sleep(1);
         }
 
-        pthread_spin_unlock(&spin);
+        pthread_spin_unlock(&spin); // освобождаем спинлок
         sleep(1);
     }
     return NULL;
 }
 
 int main() {
-    pthread_t id1, id2;
+    pthread_t id1, id2; // идентификаторы потоков
 
-    pthread_spin_init(&spin, PTHREAD_PROCESS_PRIVATE);
+    pthread_spin_init(&spin, PTHREAD_PROCESS_PRIVATE); // инициализация спинлока для потоков внутри процесса
 
-    pthread_create(&id1, NULL, proc1, NULL);
-    pthread_create(&id2, NULL, proc2, NULL);
+    pthread_create(&id1, NULL, proc1, NULL); // создание первого потока
+    pthread_create(&id2, NULL, proc2, NULL); // создание второго потока
 
-    getchar();
-    puts("\nКлавиша нажата.");
+    getchar(); // ждем нажатия клавиши пользователем
+    puts("\nКлавиша нажата."); // сообщение о том, что клавиша нажата
 
-    flag1 = 1;
+    flag1 = 1; // устанавливаем флаги, чтобы потоки завершились
     flag2 = 1;
 
-    pthread_join(id1, NULL);
-    pthread_join(id2, NULL);
+    pthread_join(id1, NULL); // ожидание завершения первого потока
+    pthread_join(id2, NULL); // ожидание завершения второго потока
 
-    pthread_spin_destroy(&spin);
-    return 0;
+    pthread_spin_destroy(&spin); // уничтожение спинлока
+    return 0; // завершение программы
 }
