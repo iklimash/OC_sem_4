a1123388c6fa7fd6b837b9ba70fcbfd22c2ba9c1
iklimash <iklimash@outlook.com>
2026-04-08 12:37:04 -0400

    v2

--- lab_7/lab7_1.c
+++ lab_7/lab7_1.c
@@ -20,51 +20,37 @@ pthread_t write_tid;
 int write_thread_created = 0;
 
 // Обработчик сигнала закрытого канала
-void sig_handler(int signo)
- {
+void sig_handler(int signo) {
     if (signo == SIGPIPE) {
         printf("[Writer] Получен сигнал SIGPIPE (читатель отсоединился).\n");
     }
 }
 
 // Поток передачи данных
-void* write_thread_func(void* arg) 
-{
+void* write_thread_func(void* arg) {
     struct passwd *pw;
     const char *username = "root"; // Имя пользователя для getpwnam
 
-    while (keep_running_write) 
-    {
+    while (keep_running_write) {
         pw = getpwnam(username);
-        if (pw == NULL) 
-        {
+        if (pw == NULL) {
             perror("Ошибка getpwnam");
             sleep(1);
             continue;
         }
 
-        // Формируем сообщение (отправляем pw_dir)
         char buffer[256];
         snprintf(buffer, sizeof(buffer), "Домашняя директория '%s': %s", username, pw->pw_dir);
 
-        // Пытаемся записать в канал
         ssize_t bytes_written = write(fifo_fd, buffer, strlen(buffer) + 1);
         
-        if (bytes_written == -1) 
-        {
-            if (errno == EPIPE) 
-            {
-                // Обработка разрыва соединения обрабатывается в sig_handler, 
-                // здесь мы можем просто подождать
-            } 
-            else if (errno != EAGAIN) 
-            {
+        if (bytes_written == -1) {
+            if (errno == EPIPE) {
+                // Сигнал обработан в sig_handler, ждем
+            } else if (errno != EAGAIN) {
                 perror("Ошибка записи write()");
             }
-        } 
-        else
-        {
-
+        } else {
             printf("[Writer] Отправлено: %s\n", buffer);
         }
 
@@ -74,39 +60,29 @@ void* write_thread_func(void* arg)
 }
 
 // Поток открытия канала
-void* open_thread_func(void* arg)
- {
-    printf("[Writer] Ожидание подключения читателя...\n");
+void* open_thread_func(void* arg) {
+    printf("[Writer] Запуск потока. Ожидание подключения читателя...\n");
     
-    while (keep_running_open) 
-    {
+    while (keep_running_open) {
         fifo_fd = open(FIFO_NAME, O_WRONLY | O_NONBLOCK);
         
-        if (fifo_fd == -1)
-         {
-            if (errno == ENXIO) 
-            {
-                // Читатель еще не открыл канал
+        if (fifo_fd == -1) {
+            if (errno == ENXIO) {
+                // ВЫВОДИМ ОШИБКУ PERROR ЗДЕСЬ
+                perror("[Writer] Ожидание читателя (ENXIO)");
                 sleep(1);
-            } 
-            else 
-            {
+            } else {
                 perror("Ошибка при открытии fifo");
                 sleep(1);
             }
-        } 
-        else
-        {
+        } else {
             printf("[Writer] Читатель подключился! Канал открыт.\n");
             
-            // Запускаем поток передачи
             if (pthread_create(&write_tid, NULL, write_thread_func, NULL) == 0) {
                 write_thread_created = 1;
             } else {
                 perror("Ошибка создания потока записи");
             }
-            
-            // Завершаем поток открытия
             break; 
         }
     }
@@ -116,16 +92,13 @@ void* open_thread_func(void* arg)
 int main() {
     pthread_t open_tid;
 
-    // Перехватываем SIGPIPE
     signal(SIGPIPE, sig_handler);
 
-    // Создаем FIFO (ошибка "файл существует" игнорируется)
     if (mkfifo(FIFO_NAME, 0666) == -1 && errno != EEXIST) {
         perror("Ошибка mkfifo");
         return 1;
     }
 
-    // Создаем поток открытия канала
     if (pthread_create(&open_tid, NULL, open_thread_func, NULL) != 0) {
         perror("Ошибка создания потока открытия");
         return 1;
@@ -134,17 +107,14 @@ int main() {
     printf("Нажмите <Enter> для завершения программы-писателя.\n");
     getchar();
 
-    // Устанавливаем флаги завершения
     keep_running_open = 0;
     keep_running_write = 0;
 
-    // Дожидаемся завершения потоков
     pthread_join(open_tid, NULL);
     if (write_thread_created) {
         pthread_join(write_tid, NULL);
     }
 
-    // Закрываем дескриптор и удаляем FIFO
     if (fifo_fd != -1) {
         close(fifo_fd);
     }
--- lab_7/lab7_2.c
+++ lab_7/lab7_2.c
@@ -15,39 +15,26 @@ volatile sig_atomic_t keep_running = 1;
 int fifo_fd = -1;
 
 // Поток чтения данных
-void* read_thread_func(void* arg) 
-{
+void* read_thread_func(void* arg) {
     char buffer[256];
 
-    while (keep_running) 
-    {
+    while (keep_running) {
         memset(buffer, 0, sizeof(buffer));
         
         ssize_t bytes_read = read(fifo_fd, buffer, sizeof(buffer) - 1);
 
-        if (bytes_read > 0) 
-        {
+        if (bytes_read > 0) {
             printf("[Reader] Получено: %s\n", buffer);
-        } 
-        else if (bytes_read == 0) 
-        {
-            // Писатель закрыл канал или еще не подключился
-            // Выводим сообщение не слишком часто, чтобы не засорять экран
-            static int disconnected_msg_shown = 0;
-            if (!disconnected_msg_shown) {
-                printf("[Reader] Писатель отсоединился (или еще не запущен). Ожидание...\n");
-                disconnected_msg_shown = 1;
-            }
+        } else if (bytes_read == 0) {
+            // Писатель не подключен. 
+            // read == 0 не устанавливает errno, поэтому perror здесь выведет "Success".
+            // Заменяем на printf, чтобы было видно, что цикл работает.
+            printf("[Reader] Писатель отсоединился (или еще не запущен). Возврат read: 0\n");
             sleep(1);
-        } 
-        else 
-        {
-            // Ошибка чтения или отсутствие данных в неблокирующем режиме
+        } else {
             if (errno == EAGAIN) {
-                // Ресурс временно недоступен (канал пуст)
-                // Сбрасываем флаг сообщения, так как писатель подключен
-                extern int disconnected_msg_shown; 
-                // (хак для сброса статической переменной в рамках примера)
+                // Выводим ошибку EAGAIN через perror
+                perror("[Reader] Нет данных (EAGAIN)");
             } else {
                 perror("Ошибка read()");
             }
@@ -60,26 +47,20 @@ void* read_thread_func(void* arg)
 int main() {
     pthread_t read_tid;
 
-    // Создаем FIFO
-    if (mkfifo(FIFO_NAME, 0666) == -1 && errno != EEXIST) 
-    {
+    if (mkfifo(FIFO_NAME, 0666) == -1 && errno != EEXIST) {
         perror("Ошибка mkfifo");
         return 1;
     }
 
-    // Открываем на чтение без блокировки
     fifo_fd = open(FIFO_NAME, O_RDONLY | O_NONBLOCK);
-    if (fifo_fd == -1) 
-    {
+    if (fifo_fd == -1) {
         perror("Ошибка при открытии fifo на чтение");
         return 1;
     }
     
     printf("[Reader] Канал открыт. Ожидание данных...\n");
 
-    // Запускаем поток чтения
-    if (pthread_create(&read_tid, NULL, read_thread_func, NULL) != 0)
-     {
+    if (pthread_create(&read_tid, NULL, read_thread_func, NULL) != 0) {
         perror("Ошибка создания потока чтения");
         return 1;
     }
@@ -87,13 +68,10 @@ int main() {
     printf("Нажмите <Enter> для завершения программы-читателя.\n");
     getchar();
 
-    // Устанавливаем флаг завершения
     keep_running = 0;
 
-    // Дожидаемся завершения потока
     pthread_join(read_tid, NULL);
 
-    // Закрываем дескриптор и удаляем FIFO
     close(fifo_fd);
     unlink(FIFO_NAME);
 
